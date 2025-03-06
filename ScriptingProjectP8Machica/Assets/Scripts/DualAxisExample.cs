3d352b53ae31b443189fa17d1e45fd52abdf7445
swagdog13 <727159@student.dusd.net>
2025-03-06 14:29:43 -0800

    Script
    I attempted to write three scripts but they didn't work :(

---
+++ ScriptingProjectP8Machica/Assets/Scripts/DualAxisExample.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DualAxisExample : MonoBehaviour
+{
+    public float hRange;
+    public float vRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * hRange;
+        float yPos = v * vRange;
+
+        transform.position = new Vector3(xPos, 0, yPos);
+        Debug.Log = h.ToString("F2");
+        Debug.Log = v.ToString("F2");
+    }
+}
