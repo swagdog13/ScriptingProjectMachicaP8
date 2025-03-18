ab345d7d8e3c30a6106daffadfcc88b360916dbd
swagdog13 <727159@student.dusd.net>
2025-03-18 14:37:03 -0700

    3 new scripts
    I added three new scripts where it allowed a gameobject to float in the air

---
+++ ScriptingProjectP8Machica/Assets/Scripts/UsingOtherComponent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class UsingOtherComponent : MonoBehaviour
+{
+    public GameObject otherGameObject;
+
+
+    private AnotherScript anotherScript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxCol;
+
+    private void Awake()
+    {
+        anotherScript = GetComponent<AnotherScript>();
+        yetAnotherScript = otherGameObject.GetComponent<YetAnotherScript>();
+        boxCol = otherGameObject.GetComponent<BoxCollider>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCol.size = new Vector3(3, 3, 3);
+        Debug.Log("The player's score is " + anotherScript.playerScore);
+        Debug.Log("The player has died " + yetAnotherScript.numberOfPlayerDeaths + " times");
+    }
+}
+
+    // Update is called once per frame
