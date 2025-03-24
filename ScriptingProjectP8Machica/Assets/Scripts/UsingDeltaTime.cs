104c3d977b9aaaf3e65e8d2b1e0c9f683eadd8fa
swagdog13 <727159@student.dusd.net>
2025-03-24 14:41:46 -0700

    new script
    I added a script where it messes up the lighting

---
+++ ScriptingProjectP8Machica/Assets/Scripts/UsingDeltaTime.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingDeltaTime : MonoBehaviour
+{
+    public float speed = 8f;
+    public float countdown = 3.0f;
+    public GameObject light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0.0f)
+        {
+            if (light != null)
+            {
+                Light lightComponent = light.GetComponent<Light>();
+                if (lightComponent != null )
+                {
+                    lightComponent.enabled = false;
+
+
+                }
+
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+    }
+
+}
+
+
